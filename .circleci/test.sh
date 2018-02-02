3bbb1c3017292c4db260ae2a8250afc0b2bff017
etienne <etienne@plot.ly>
2018-02-01 19:20:11 -0500

    move bundle tests out of jasmine test job

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -8,16 +8,20 @@ EXIT_STATE=0
 
 case $1 in
 
-    jasmine)
-        npm run test-jasmine    || EXIT_STATE=$?
-        npm run test-bundle     || EXIT_STATE=$?
-        exit $EXIT_STATE
-        ;;
-
     image)
         npm run test-image      || EXIT_STATE=$?
         npm run test-export     || EXIT_STATE=$?
         npm run test-image-gl2d || EXIT_STATE=$?
+        exit $EXIT_STATE
+        ;;
+
+    jasmine)
+        npm run test-jasmine    || EXIT_STATE=$?
+        exit $EXIT_STATE
+        ;;
+
+    bundle)
+        npm run test-bundle     || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
 
